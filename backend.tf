7e40710004d84e6bbd9c157a6fa7cd9f8684ab1b
MalharKt <malharkauthale2019@gmail.com>
2026-03-12 05:50:31 +0000

    added oidc

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-terraform-state-0002"
+    bucket         = "eks-terraform-state-0001"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
